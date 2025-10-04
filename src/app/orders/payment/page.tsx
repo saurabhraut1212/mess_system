dde7bf7c0a073e9ce7b67e96ed5161157e3bf1a8
saurabhraut1212 <saurabhrmahadev1212@gmail.com>
2025-10-04 13:43:54 +0530

    fix: updated payment module

--- src/app/orders/payment/page.tsx
+++ src/app/orders/payment/page.tsx
@@ -1,32 +1,43 @@
-'use client';
+import React from 'react'
 
-import PaymentForm from '@/components/PaymentForm';
-import { useSearchParams, useRouter } from 'next/navigation';
-import toast, { Toaster } from 'react-hot-toast';
-import { useEffect, useState } from 'react';
-
-export default function PaymentPage() {
-  const searchParams = useSearchParams();
-  const orderIdParam = searchParams.get('orderId');
-  const [orderId, setOrderId] = useState('');
-  const router = useRouter();
-
-  useEffect(() => {
-    if (!orderIdParam) {
-      toast.error('Order ID missing');
-    } else {
-      setOrderId(orderIdParam);
-    }
-  }, [orderIdParam]);
-
-  if (!orderId) {
-    return <p className="text-center mt-10">Invalid order</p>;
-  }
-
+const page = () => {
   return (
-    <div>
-      <Toaster position="top-right" />
-      <PaymentForm orderId={orderId} onSuccess={() => router.push('/orders')} />
-    </div>
-  );
+    <div>page</div>
+  )
 }
+
+export default page
+
+
+// 'use client';
+
+// import PaymentForm from '@/components/PaymentForm';
+// import { useSearchParams, useRouter } from 'next/navigation';
+// import toast, { Toaster } from 'react-hot-toast';
+// import { useEffect, useState } from 'react';
+
+// export default function PaymentPage() {
+//   const searchParams = useSearchParams();
+//   const orderIdParam = searchParams.get('orderId');
+//   const [orderId, setOrderId] = useState('');
+//   const router = useRouter();
+
+//   useEffect(() => {
+//     if (!orderIdParam) {
+//       toast.error('Order ID missing');
+//     } else {
+//       setOrderId(orderIdParam);
+//     }
+//   }, [orderIdParam]);
+
+//   if (!orderId) {
+//     return <p className="text-center mt-10">Invalid order</p>;
+//   }
+
+//   return (
+//     <div>
+//       <Toaster position="top-right" />
+//       <PaymentForm orderId={orderId} onSuccess={() => router.push('/orders')} />
+//     </div>
+//   );
+// }
