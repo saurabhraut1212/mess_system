6acc6c83e16d39fdbd0836e52bea2a30242401c2
saurabhraut1212 <saurabhrmahadev1212@gmail.com>
2025-10-05 09:54:17 +0530

    fix: orders list with updating with lean

--- src/app/api/orders/list/route.ts
+++ src/app/api/orders/list/route.ts
@@ -1,27 +1,42 @@
 import { NextRequest, NextResponse } from 'next/server';
-import {connectDB} from '@/lib/db';
+import { connectDB } from '@/lib/db';
 import Order from '@/models/Order';
 import { verifyToken } from '@/lib/auth';
 
 interface DecodedToken {
-    id: string;
-    email: string;
-    role: string;
+  id: string;
+  email: string;
+  role: string;
 }
+
 export async function GET(req: NextRequest) {
   await connectDB();
   const authHeader = req.headers.get('Authorization');
-  if (!authHeader) return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+  if (!authHeader)
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
 
   try {
-   const token = authHeader.split(' ')[1];
-   const decoded = verifyToken(token) as DecodedToken;
+    const token = authHeader.split(' ')[1];
+    const decoded = verifyToken(token) as DecodedToken;
 
+    let orders;
 
-    const orders = await Order.find({user:decoded.id}).populate('items.menuId').sort({ createdAt: -1 });
+    // 🧠 Role-based logic
+    if (decoded.role === 'customer') {
+      // Fetch only user's orders
+      orders = await Order.find({ user: decoded.id })
+        .populate('items.menuId')
+        .sort({ createdAt: -1 }).lean();
+    } else {
+      // Fetch all orders (admin / manager)
+      orders = await Order.find()
+        .populate('items.menuId')
+        .sort({ createdAt: -1 }).lean();
+    }
+
     return NextResponse.json(orders);
   } catch (err) {
-    console.log(err);
+    console.error('Error fetching orders:', err);
     return NextResponse.json({ error: 'Server error' }, { status: 500 });
   }
 }
